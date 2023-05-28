3fe6f93f7da29c68f8be4bb1199873b0dd85a7fb
Ihury <ihurykc@gmail.com>
2023-05-28 16:15:47 -0300

    Alteração na forma de leitura do número. (agora os números são lidos como strings)

--- main.c
+++ main.c
@@ -1,56 +1,60 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "lista.h"
+
+void lerNumero(BigInt *l);
 
 int main()
 {
     BigInt *l1 = criar();
-
-    int x;
-
-    do
-    {
-        scanf("%d", &x);
-
-        if (x < 10 && x > -10)
-        {
-            if (inserirInicio(l1, x))
-                return 1; // se o número for de apenas um digito, ele adiciona no numero, senão ele sai
-        }
-    } while (x < 10 && x > -10);
-
-    removeZero(l1);    // remove os zeros a esquerda
-    converteSinal(l1); // se o primeiro numero digitado for negativo, transforma os outros digitos em negativos
-    mostrar(l1);
-
     BigInt *l2 = criar();
-
-    do
-    {
-        scanf("%d", &x);
-
-        if (x < 10 && x > -10)
-        {
-            if (inserirInicio(l2, x))
-                return 1; // se o número for de apenas um digito, ele adiciona no numero, senão ele sai
-        }
-    } while (x < 10 && x > -10);
-
-    removeZero(l2);    // remove os zeros a esquerda
-    converteSinal(l2); // se o primeiro numero digitado for negativo, transforma os outros digitos em negativos
-    mostrar(l2);       // mostra o inteiro grande
-
     BigInt *l3 = criar();
+    BigInt *l4 = criar();
+
+    lerNumero(l1);
+    lerNumero(l2);
 
     subtracao(l1, l2, l3);
     removeZero(l3);
     mostrar(l3);
 
+    soma(l1, l2, l4);
+    removeZero(l4);
+    mostrar(l4);
+
     limpar(l1);
     limpar(l2);
     limpar(l3);
+    limpar(l4);
 
     system("pause");
 
     return 0;
+}
+
+void lerNumero(BigInt *l)
+{
+    char input[100]; // tamanho máximo da entrada
+
+    printf("Digite um numero:\n");
+    scanf("%s", input);
+
+    int length = strlen(input);
+    for (int i = 0; i < length; i++)
+    {
+        int digit = input[i] - '0'; // converte o caractere para inteiro
+
+        if (digit < 10 && digit > -10)
+        {
+            if (inserirInicio(l, digit))
+            {
+                return; // se o número for de apenas um digito, ele adiciona no numero, senão ele sai
+            }
+        }
+    }
+
+    removeZero(l);    // remove os zeros a esquerda
+    converteSinal(l); // se o primeiro numero digitado for negativo, transforma os outros digitos em negativos
+    mostrar(l);
 }
